54576f98b419dda87f6834b1842a7bbd7b3f3955
Kerry Guarino <kerry.guarino@gmail.com>
2023-08-24 21:46:24 -0400

    while loop and list

--- lib/looping.py
+++ lib/looping.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = [square_integers * square_integers for square_integers in int_list]
+    return squared
 
 def fizzbuzz():
     # code goes here!
